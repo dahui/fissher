327c64f3a724d78e805ac3af8840ed28fdf8126f
Jeff Hagadorn <admin@digital-apathy.net>
2012-04-17 10:02:37 -0400

    Refactor hashes to incorporate symbols. Moar bedder!

--- lib/fissher_base.rb
+++ lib/fissher_base.rb
@@ -10,31 +10,31 @@ class FissherBase
   def go_time
     opts = FissherConf.handle_opts unless !opts.nil?
 
-    Net::SSH::Multi.start(:concurrent_connections => opts["concurrency"]) do |session|
-      if opts["gateway"]
-        session.via opts["gateway"], opts["user"], :password => opts["password"]
+    Net::SSH::Multi.start(:concurrent_connections => opts[:concurrency]) do |session|
+      if opts[:gateway]
+        session.via opts[:gateway], opts[:user], :password => opts[:password]
       end
 
       # Create our connection list
-      abort "No hosts specified! Please use -H or -G!\n" unless !opts["hostlist"].nil?
-      opts["hostlist"].each do |host|
-        session.use host, :user => opts["user"], :password => opts["password"]
+      abort "No hosts specified! Please use -H or -G!\n" unless !opts[:hostlist].nil?
+      opts[:hostlist].each do |host|
+        session.use host, :user => opts[:user], :password => opts[:password]
       end
 
-      if opts["command"] =~ /^sudo/
-        if opts["password"].nil?
+      if opts[:command] =~ /^sudo/
+        if opts[:password].nil?
           p = FissherConf::Misc.new
-          opts["password"] = p.getpass
+          opts[:password] = p.getpass
         end
 
         # Get a PTY and exec command on servers
         session.open_channel do |ch|
           ch.request_pty do |c, success|
             raise "could not request pty" unless success
-            ch.exec opts["command"]
+            ch.exec opts[:command]
             ch.on_data do |c_, data|
               if data =~ /\[sudo\]/
-                ch.send_data(opts["password"] + "\n")
+                ch.send_data(opts[:password] + "\n")
               else
                 puts "[#{ch[:host]}]: #{data}" unless data =~ /^\n$|^\s*$/
               end
@@ -43,10 +43,10 @@ class FissherBase
         end
       else
         # Sudo isn't needed. We don't need a PTY.
-        session.exec(opts["command"]) 
+        session.exec(opts[:command]) 
       end
 
-      # go time... for real this time.
+      # go time... for real.
       session.loop
     end
   end
--- lib/fissher_conf.rb
+++ lib/fissher_conf.rb
@@ -71,65 +71,65 @@ module FissherConf
 
     # Gateway if an edgeserver is present
     if opt["g"]
-      ret["gateway"] = opt["g"]
+      ret[:gateway] = opt["g"]
     elsif opt["G"] && !config["hostgroups"]["#{opt['G']}"]["gateway"].nil?
-      ret["gateway"] = config["hostgroups"]["#{opt['G']}"]["gateway"]
+      ret[:gateway] = config["hostgroups"]["#{opt['G']}"]["gateway"]
     elsif !config['default_gateway'].nil?
-      ret["gateway"] = config['default_gateway']
+      ret[:gateway] = config['default_gateway']
     end  
 
     # Hostgroup used for batch jobs
     if opt["G"]
       abort "You may only specify one of -G or -H!\n" unless opt["H"].nil?
       h = Misc.new
-      ret["hostlist"] = h.group_hosts(opt["G"],config,conf_file) 
+      ret[:hostlist] = h.group_hosts(opt["G"],config,conf_file) 
     end
 
     # Username used by connections
     if opt["u"]
-      ret["user"] = opt['u']
+      ret[:user] = opt['u']
     elsif config['user']
-      ret["user"] = config['user']
+      ret[:user] = config['user']
     end
   
     # Job Concurrency limit
     if opt["n"] == '0'
-      ret["concurrency"] = nil
+      ret[:concurrency] = nil
     elsif opt["n"]
-      ret["concurrency"] = opt["n"].to_i
-    elsif config['concurrency']
-      ret["concurrency"] = config['concurrency'].to_i
+      ret[:concurrency] = opt["n"].to_i
+    elsif config[:concurrency]
+      ret[:concurrency] = config['concurrency'].to_i
     else
-      ret["concurrency"] = 10
+      ret[:concurrency] = 10
     end
 
     # Host list
     if opt["H"]
       abort "You may only specify one of -G or -H!\n" unless opt["G"].nil?
       if opt["H"] =~ /,/
-        ret["hostlist"] = opt["H"].split(",")
+        ret[:hostlist] = opt["H"].split(",")
       else
-        ret["hostlist"] = [ opt["H"] ]
+        ret[:hostlist] = [ opt["H"] ]
       end
     end
 
     # Get our account password
     if opt["p"]
       p = Misc.new
-      ret["password"] = p.getpass()
+      ret[:password] = p.getpass()
     else
-      ret["password"] = nil
+      ret[:password] = nil
     end
     
     # Our command
     if ARGV.count >= 1
       if sudo_cmd
-        ret["command"] = "#{sudo_cmd} " + ARGV.join(' ').to_s
+        ret[:command] = "#{sudo_cmd} " + ARGV.join(' ').to_s
       else
-        ret["command"] = ARGV.join(' ').to_s 
+        ret[:command] = ARGV.join(' ').to_s 
       end
     else
-      die "No command specified!\n" unless !ret["command"].nil?
+      die "No command specified!\n" unless !ret[:command].nil?
     end
     ret
   end
